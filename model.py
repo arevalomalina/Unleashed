05f8a2030029fe77340b42eada54a93b0258b039
arevalomalina <Malina@Malinas-MacBook-Air.local>
2014-07-08 13:07:21 -0700

    walker_webapp first commit.

---
+++ model.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+### Class declarations go here
+
+
+### End class declarations
+
+def main():
+    """In case we need this for something"""
+    pass
+
+if __name__ == "__main__":
+    main()
